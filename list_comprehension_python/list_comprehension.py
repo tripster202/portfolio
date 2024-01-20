dc0ae4a98b52da878146baefabfc363bcdaceef2
tripster202 <hockeyco28@gmail.com>
2024-01-20 15:52:07 -0500

    functional. note credits to Kaggle lesson

--- list_comprehension_python/list_comprehension.py
+++ list_comprehension_python/list_comprehension.py
@@ -1,7 +1,10 @@
 # IMPORTS #
-# None
+import random
 
-def run_list_comprehension():
+# NOTES #
+# https://www.kaggle.com/code/colinmorris/loops-and-list-comprehensions
+
+def squares():
     squares = [n**2 for n in range(10)]
     print(squares)
 
@@ -10,5 +13,17 @@ def run_list_comprehension():
         squares.append(n**2)
     print(squares)
 
+def planets():
+    planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+    short_planets = [planet for planet in planets if len(planet) < 6]
+    print(short_planets)
+
+def random_list(length):
+    if 0 < length <= 10:
+        print([random.randint(10,99) for idx in range(length)])
+    else: print(list())
+
 if __name__ == "__main__":
-    run_list_comprehension()
+    squares()
+    planets()
+    random_list(10)
