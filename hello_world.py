a880264c75b4c722e8a38f32e6d394aef41c4802
tripster202 <hockeyco28@gmail.com>
2024-01-20 12:31:44 -0500

    testing complete. functional.

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+def hello_world():
+    print(f"Hello World!")
+    user_name = input("What is your name?\n")
+    print(f"Hello {user_name}!")
+    print("Goodbye!")
+
+if __name__ == "__main__":
+    hello_world()
